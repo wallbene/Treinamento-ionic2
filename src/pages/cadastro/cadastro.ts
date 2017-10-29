8077374b3c7d6e5b43ca344e3dd7cdeb9676d8bb
wallbene <wallacebenevides@hotmail.com>
2017-10-29 21:18:48 -0200

    formulário de cadastro

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -10,10 +10,22 @@ export class CadastroPage {
   public precoTotal: number;
   public carro: Carro;
 
+  public nome: string;
+  public endereco: string;
+  public email: string;
+  public data: string = new Date().toISOString();
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
     this.carro = this.navParams.get('carro');
     this.precoTotal = this.navParams.get('precoTotal');
+  }
+
+  agendar(): void {
+    console.log(this.nome);
+    console.log(this.endereco);
+    console.log(this.email);
+    console.log(this.data);
   }
   
 
